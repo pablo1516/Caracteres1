8124fd4efc31595aa700302309fa40cf91499ab7
pablo1516 <61156436+pablo1516@users.noreply.github.com>
2020-04-04 10:52:11 -0500

    Add files via upload

---
+++ Ejerciciocaracteres1/src/ejerciciocaracteres1/Ejerciciocaracteres1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciocaracteres1;
+
+/**
+ *
+ * @author david
+ */
+public class Ejerciciocaracteres1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmEjerciciocaracteres1 frmcarac = new FrmEjerciciocaracteres1();
+        frmcarac.setVisible(true);
+        frmcarac.setLocationRelativeTo(null);
+        frmcarac.setSize(260, 220);
+        frmcarac.setResizable(false);
+        frmcarac.setTitle("Caracteres 1");
+    }
+    
+}
